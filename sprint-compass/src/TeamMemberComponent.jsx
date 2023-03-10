f4a3337b6fc8736bb68132f04142c1ee58db2561
Amber Rose <a_rose2@fanshaweonline.ca>
2023-03-10 12:31:41 -0500

    team member comp works

--- sprint-compass/src/TeamMemberComponent.jsx
+++ sprint-compass/src/TeamMemberComponent.jsx
@@ -8,6 +8,10 @@ import {
 	TextField,
 	Button,
 	Modal,
+	List,
+	ListItem,
+	ListItemText,
+	Divider,
 } from "@mui/material";
 import AddCircle from "@mui/icons-material/AddCircle";
 import theme from "./theme";
@@ -16,37 +20,94 @@ import HomeComponent from "./HomeComponent";
 
 const TeamMemberComponent = (props) => {
 	const initialState = {
-		teamArray: [],
+		//test data for ui dev
+		teamArray: ["Gavin G", "Ryan M", "Amber R"],
+		// teamArray: [],
 		showAddCard: false,
-		firstName: "",
-		lastName: "",
+		newName: "",
 	};
 	const reducer = (state, newState) => ({ ...state, ...newState });
 	const [state, setState] = useReducer(reducer, initialState);
-	const onAddClicked = async () => {
-		let teamMember = {
-			firstName: state.firstName,
-			lastName: state.lastName,
-		};
+	const onCancelClicked = () => {
+		closeModal();
 	};
+	const onAddClicked = async () => {
+		//todo: code to add team member to db
+		//
+		//reset name
+		setState({ newName: "" });
+		closeModal();
+	};
+	const showModal = () => {
+		setState({ showAddCard: true });
+	};
+	const closeModal = () => {
+		setState({ showAddCard: false });
+	};
+	const handleNewNameInput = (e) => {
+		setState({ newName: e.target.value });
+	};
+	const emptyorundefined = state.newName === undefined || state.newName === "";
 	return (
 		<ThemeProvider theme={theme}>
 			<Card className="card">
 				<HomeComponent />
+				<Modal open={state.showAddCard}>
+					<Card className="card">
+						<CardHeader
+							title="Add Team Member"
+							style={{ color: theme.palette.primary.main, textAlign: "center" }}
+						/>
+						<CardContent>
+							<div style={{ textAlign: "center" }}>
+								<TextField
+									style={{ margin: "1vw" }}
+									onChange={handleNewNameInput}
+									placeholder="Team Member Name"
+									value={state.newName}
+								/>
+							</div>
+							<div style={{ textAlign: "center" }}>
+								<Button
+									color="secondary"
+									variant="contained"
+									onClick={onCancelClicked}
+								>
+									Cancel
+								</Button>
+								<Button
+									color="secondary"
+									variant="contained"
+									onClick={onAddClicked}
+									disabled={emptyorundefined}
+								>
+									Add
+								</Button>
+							</div>
+						</CardContent>
+					</Card>
+				</Modal>
 				<CardHeader
 					title="Team Members"
 					style={{ color: theme.palette.primary.main, textAlign: "center" }}
 				/>
 				<CardContent>
-					<Modal isOpen={state.showAddCard}>
-						<div>
-							<p>test</p>
-						</div>
-					</Modal>
+					<List style={{ color: theme.palette.error.main }}>
+						{state.teamArray.map((name, index) => {
+							return (
+								<div key={index}>
+									<ListItem>
+										<ListItemText primary={name} />
+									</ListItem>
+									<Divider />
+								</div>
+							);
+						})}
+					</List>
 					<IconButton
 						color="secondary"
 						style={{ marginTop: 50, float: "right" }}
-						// onClick={}
+						onClick={showModal}
 					>
 						<AddCircle fontSize="large" />
 					</IconButton>
