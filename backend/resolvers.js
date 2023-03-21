703140fde7b941bea7a01e43c4dc154c3a74916d
Gavin Guerette <gavin.guerette@hotmail.com>
2023-03-21 14:48:16 -0400

    Updated resolvers

--- backend/resolvers.js
+++ backend/resolvers.js
@@ -123,6 +123,119 @@ const resolvers = {
     let results = await rtn.addOne(db, cfg.taskColl, task);
     return results.acknowledged ? task : null;
   },
+  updateuser: async (args) => {
+    let db = await rtn.getDBInstance();
+    let results = await rtn.updateOne(
+      db,
+      cfg.userColl,
+      { _id: new rtn.ObjectId(args._id) },
+      { name: args.name, role: args.role }
+    );
+    return results.value !== null
+      ? args
+      : {
+          _id: args._id,
+          name: "None found",
+          role: "No user updated",
+        };
+  },
+  updatetask: async (args) => {
+    let db = await rtn.getDBInstance();
+    let results = await rtn.updateOne(
+      db,
+      cfg.taskColl,
+      { _id: new rtn.ObjectId(args._id) },
+      {
+        taskname: args.taskname,
+        storyid: args.storyid,
+        taskdetails: args.taskdetails,
+        teammember: args.teammember,
+        hourscompleted: args.hourscompleted,
+        iscompleted: args.iscompleted,
+      }
+    );
+    return results.value !== null
+      ? args
+      : {
+          _id: args._id,
+          taskname: "None found",
+          taskdetails: "No task updated",
+        };
+  },
+  updatestory: async (args) => {
+    let db = await rtn.getDBInstance();
+    let results = await rtn.updateOne(
+      db,
+      cfg.storyColl,
+      { _id: new rtn.ObjectId(args._id) },
+      {
+        storyname: args.storyname,
+        storydescription: args.storydescription,
+        sprints: args.sprints,
+        storypoints: args.storypoints,
+        costperhour: args.costperhour,
+        priority: args.priority,
+        tasks: args.tasks,
+      }
+    );
+    return results.value !== null
+      ? args
+      : {
+          _id: args._id,
+          storyname: "None found",
+          storydescription: "No story updated",
+        };
+  },
+  updateproduct: async (args) => {
+    let db = await rtn.getDBInstance();
+    let results = await rtn.updateOne(
+      db,
+      cfg.productColl,
+      { _id: new rtn.ObjectId(args._id) },
+      {
+        productname: args.productname,
+        teamname: args.teamname,
+        startdate: args.startdate,
+        enddate: args.enddate,
+        productowner: args.productowner,
+        teammembers: args.teammembers,
+        hoursperstorypoint: args.hoursperstorypoint,
+        estimatestorypoints: args.estimatestorypoints,
+        estimatetotalcost: args.estimatetotalcost,
+        sprints: args.sprints,
+      }
+    );
+    return results.value !== null
+      ? args
+      : {
+          _id: args._id,
+          productname: "None found",
+          teamname: "No product updated",
+        };
+  },
+  updatesprint: async (args) => {
+    let db = await rtn.getDBInstance();
+    let results = await rtn.updateOne(
+      db,
+      cfg.sprintColl,
+      { _id: new rtn.ObjectId(args._id) },
+      {
+        productid: args.productid,
+        sprintname: args.sprintname,
+        startdate: args.startdate,
+        enddate: args.enddate,
+        iscompleted: args.iscompleted,
+        stories: args.stories,
+      }
+    );
+    return results.value !== null
+      ? args
+      : {
+          _id: args._id,
+          productid: "None found",
+          sprintname: "No sprint updated",
+        };
+  },
 };
 
 export default resolvers;
